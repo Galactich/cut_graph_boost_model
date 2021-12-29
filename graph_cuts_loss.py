7e26737b8a1feb0150d6077d139bb061a6c3f0b8
Galactich <159463319+Galactich@users.noreply.github.com>
2021-12-29 19:14:41 +0000

    f2fs: check the page status filled from disk

--- graph_cuts_loss.py
+++ graph_cuts_loss.py
@@ -10,3 +10,8 @@ class GC_2D_Original(torch.nn.Module):
     def __init__(self, lmda, sigma):
         super(GC_2D_Original, self).__init__()
         self.lmda = lmda
+        self.sigma = sigma
+
+    def forward(self, input, target):
+        # input: B * C * H * W, after sigmoid operation
+        # target: B * C * H * W
