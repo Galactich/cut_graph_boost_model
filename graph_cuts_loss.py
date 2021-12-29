2cc9d376284077eb347a96eb5e32e22e882b6a18
Galactich <159463319+Galactich@users.noreply.github.com>
2021-12-29 10:00:22 +0000

    3.2.9

--- graph_cuts_loss.py
+++ graph_cuts_loss.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 import torch
 import torch.nn
+
+
+# original 2D GC loss with no approximation
+class GC_2D_Original(torch.nn.Module):
+
+    def __init__(self, lmda, sigma):
+        super(GC_2D_Original, self).__init__()
+        self.lmda = lmda
