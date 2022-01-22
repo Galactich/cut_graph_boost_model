1aa74e97135b061923774d45133f3369c9a44e5e
Galactich <159463319+Galactich@users.noreply.github.com>
2022-01-22 17:55:49 +0000

    Input: usbtouchscreen - avoid unresponsive TSC-30 touch screen

--- graph_cuts_loss.py
+++ graph_cuts_loss.py
@@ -86,3 +86,8 @@ class GC_2D(torch.nn.Module):
 
         # horizontal: x <-> x2, x4 <-> x
         target_hori = torch.abs(target[:, :, :, 1:] - target[:, :, :, :-1])  # delta(yu, yv)
+        input_hori = torch.abs(input[:, :, :, 1:] - input[:, :, :, :-1])  # |pu - pv|
+
+        # diagonal1: x <-> x5, x8 <-> x
+        target_diag1 = torch.abs(target[:, :, 1:, 1:] - target[:, :, :-1, :-1])  # delta(yu, yv)
+        input_diag1 = torch.abs(input[:, :, 1:, 1:] - input[:, :, :-1, :-1])  # |pu - pv|
